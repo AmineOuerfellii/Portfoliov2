5a12fe160a0d75ef1c54b69b41064c1d88f9d204
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-10-05 23:07:47 +0100

    C-2

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    allowedHosts: [
+      '7966d48c-4f46-4d61-a977-e4c66bdd4518-00-17v0jb6bwlsrk.kirk.replit.dev'
+    ]
+  }
 });
